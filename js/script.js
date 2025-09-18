6059727a6e9341b214e3e3137478b046f8f5970a
gustavords <22622146+gustavords@users.noreply.github.com>
2025-09-18 16:18:49 -0400

    Create paint() to change colour on mouseover

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+
 function createGrid(size) {
     const grid = document.querySelector(`.grid`);
     let width = grid.offsetWidth;
@@ -10,9 +12,27 @@ function createGrid(size) {
         cell.style.flexBasis = `${width/size}px`;
         cell.style.boxSizing = `border-box`;
         cell.style.border = `1px red solid`;
+        cell.classList.add(`cell`);
         grid.appendChild(cell);
         total_cells--;
     }
 }
 
-createGrid(64);
+function resizeGrid(){
+    let size = prompt(`Write an even number from 12-64 for grid size`);
+    createGrid(size);
+}
+
+function paint(colour){
+    const cells = document.querySelectorAll(`.grid`);
+    
+    cells.forEach((cell)=>{
+        cell.addEventListener(`mouseover`,(e)=>{
+             e.target.style.backgroundColor =  `blue`;
+        });
+    });
+    
+}
+
+createGrid(12);
+paint();
